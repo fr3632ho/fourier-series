980095ce03153e0d972f3d0a2f3c70d45105a778
fr3632ho <fredrik@dannert.se>
2020-03-11 09:33:38 +0100

    added features to complex

--- src/Complex.py
+++ src/Complex.py
@@ -1,7 +1,17 @@
+import math
 
 class Complex:
 
-    def __init__(self,re,im):
+    def __init__(self,re,im,freq):
         self.re = re
         self.im = im
-        
+        self.freq = freq
+
+    def amp():
+        return math.sqrt(self.re * self.re + self.im*self.im)
+
+    def phase():
+        return math.atan2(self.im/self.re)
+
+    def frequency():
+        return self.freq
